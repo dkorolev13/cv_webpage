776d064148095d72e1bd0407dbd0fb534f2d5c45
Dmitry Korolev <dkorolev13@gmail.com>
2022-04-06 12:27:46 +0300

    Worked version

--- hello.py
+++ hello.py
@@ -2,10 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello Flask'
 
-@app.route('/cv')
+@app.route('/')
 def show_cv():
     return render_template("index.html")
